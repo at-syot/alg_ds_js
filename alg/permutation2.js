17264f933e349d315a2a1546011505c3c338c586
Todsapon Boontap <todsapon.boontap@accenture.com>
2022-03-28 15:59:28 +0700

    add permutation2, twopointers, treeMin.

--- alg/permutation2.js
+++ alg/permutation2.js
@@ -1,6 +1,7 @@
 let nums = [1, 2, 3];
 
 {
+  // first try
   const buildUsableNums = (nums) => {
     let usableNums = {};
     for (let n of nums) {
@@ -64,4 +65,65 @@ let nums = [1, 2, 3];
 
     return solve(nums, usableNums, state, paths, solutions);
   };
+}
+
+{
+  // second try -> permutation with group or number only # first stage #1
+
+  // done
+  const makeHashedNums = (nums) =>
+    nums.reduce((pre, curr, _, arr) => {
+      curr = Number(curr);
+      pre[curr] = arr.filter((n) => n == curr).length;
+      return pre;
+    }, {});
+
+  const validState = (state, goal) => state.length == goal;
+  const makeCandidates = (state, nums, hashedNums) => {
+    hashedNums = { ...hashedNums };
+    if (state.length == 0) {
+      // first state
+      // candidates will have only distincted values
+      return Object.keys(hashedNums).map((k) => Number(k));
+    }
+
+    let candidates = [];
+    for (let n of nums) {
+      n = Number(n);
+      let existingLen = state.filter((i) => i == n).length;
+      if (existingLen < hashedNums[n] || existingLen == 0) {
+        candidates.push(n);
+        hashedNums[n] -= 1;
+      }
+    }
+    return candidates;
+  };
+
+  const solve = (nums, hashedNums, state, solutions, level) => {
+    if (validState(state, nums.length)) {
+      cpState = state.slice();
+      cpStateStr = cpState.join(",");
+      if (!solutions.has(cpStateStr)) solutions.add(cpStateStr);
+    }
+    let candidates = makeCandidates(state, nums, hashedNums);
+
+    for (candidate of candidates) {
+      state.push(candidate);
+      solve(nums, hashedNums, state, solutions, level + 1);
+      state.pop();
+    }
+
+    return solutions;
+  };
+
+  let nums = [1, 1, 3];
+  let state = [];
+  let solutions = new Set();
+  let hashedNums = makeHashedNums(nums);
+  solve(nums, hashedNums, state, solutions, 0);
+
+  let response = [];
+  for (let solution of solutions) {
+    response.push(solution.split(",").map((n) => Number(n)));
+  }
 }
--- binary_tree/tree_min.js
+++ binary_tree/tree_min.js
@@ -1,53 +1,57 @@
 class Node {
-	constructor(v) {
-		this.value = v
-		this.left = null
-		this.right = null
-	}
+  constructor(v) {
+    this.value = v;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-let five = new Node(5)
-let eleven = new Node(11)
-let tree = new Node(3)
-let four = new Node(4)
-let anotherFive = new Node(5)
-let twelve = new Node(12)
+let five = new Node(5);
+let eleven = new Node(11);
+let tree = new Node(3);
+let four = new Node(4);
+let anotherFive = new Node(5);
+let twelve = new Node(12);
 
-five.left = eleven
-five.right = tree
-eleven.left = four
-eleven.right = anotherFive
-tree.right = twelve
+five.left = eleven;
+five.right = tree;
+eleven.left = four;
+eleven.right = anotherFive;
+tree.right = twelve;
 
-{ // dfs
-	const minTree = (node) => {
-		if (!node) return 0
+{
+  // dfs
+  const minTree = (node) => {
+    if (!node) return 0;
 
-		let v = node.value
-		let left = minTree(node.left); let right = minTree(node.right)
-		if (left > 0 && left < v) v = left;
-		if (right > 0 && right < v) v = right;
+    let v = node.value;
+    let left = minTree(node.left);
+    let right = minTree(node.right);
+    if (left > 0 && left < v) v = left;
+    if (right > 0 && right < v) v = right;
 
-		return v
-	}
+    return v;
+  };
 
-	let min = minTree(five); min
+  let min = minTree(five);
+  min;
 }
 
-{ // bfs
-	const minTree = (node) => {
-		if (!node) return 0
+{
+  // bfs
+  const minTree = (node) => {
+    if (!node) return 0;
 
-		let min = node.value
-		let queue = [node]
-		while (queue.length > 0) {
-			let curr = queue.shift()
-			if (curr.value < min) min = curr.value;
+    let min = node.value;
+    let queue = [node];
+    while (queue.length > 0) {
+      let curr = queue.shift();
+      if (curr.value < min) min = curr.value;
 
-			if (curr.left) queue.push(curr.left)
-			if (curr.right) queue.push(curr.right)
-		}
+      if (curr.left) queue.push(curr.left);
+      if (curr.right) queue.push(curr.right);
+    }
 
-		return min
-	}
+    return min;
+  };
 }
